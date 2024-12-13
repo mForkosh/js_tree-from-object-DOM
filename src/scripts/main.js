b03cd53ebe8d7fc26d6b0a465221bf1719760155
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-13 13:53:06 +0200

    v0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (element === null) {
+    document.body.append(createTree(true, data));
+  } else {
+    const ulList = document.createElement('ul');
+
+    for (const key of Object.keys(data)) {
+      const ulListRow = document.createElement('li');
+
+      ulListRow.textContent = key;
+      ulListRow.append(createTree(element, data[key]));
+      ulList.append(ulListRow);
+    }
+
+    return ulList;
+  }
 }
 
 createTree(tree, food);
